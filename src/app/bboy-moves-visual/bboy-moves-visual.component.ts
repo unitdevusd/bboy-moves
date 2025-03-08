c0274ba744c729553b416be785104580ba272add
unitdevusd <unitstoragedevelopment.gmail.com>
2025-03-08 14:56:19 +0530

    initial model open code

--- src/app/bboy-moves-visual/bboy-moves-visual.component.ts
+++ src/app/bboy-moves-visual/bboy-moves-visual.component.ts
@@ -1,5 +1,5 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'bboy-moves-visual',
@@ -8,5 +8,24 @@ import { Component } from '@angular/core';
 })
 
 export class BboyMovesVisualComponent {
-    
+  private modal: any;
+
+  ngAfterViewInit() {
+    const modalElement = document.getElementById('myModal');
+    if (modalElement) {
+      this.modal = new bootstrap.Modal(modalElement);
+    }
+  }
+
+  openModal() {
+    if (this.modal) {
+      this.modal.show();
+    }
+  }
+
+  closeModal() {
+    if (this.modal) {
+      this.modal.hide();
+    }
+  }
 }
